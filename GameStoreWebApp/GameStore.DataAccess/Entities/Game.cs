20e894ed0609aa2bd8a20e9bab51962accf34fc3
Saidabror Siddiqjonov <siddiqjonovsaidabror@gmail.com>
2025-05-04 05:47:15 +0500

    Entities were added

---
+++ GameStoreWebApp/GameStore.DataAccess/Entities/Game.cs
@@ -0,0 +1,12 @@
+﻿namespace GameStore.DataAccess.Entities;
+
+public class Game
+{
+    public required Guid GameId { get; set; }
+    public required string Name { get; set; }
+    public required string Key { get; set; }
+    public string? Description { get; set; }
+
+    public ICollection<GameGenre> GameGenres { get; set; }
+    public ICollection<GamePlatform> GamePlatforms { get; set; }
+}
---
+++ GameStoreWebApp/GameStore.DataAccess/Entities/Genre.cs
@@ -0,0 +1,12 @@
+﻿namespace GameStore.DataAccess.Entities;
+
+public class Genre
+{
+    public required Guid GenreId { get; set; }
+    public required string Name { get; set; }
+
+    public Guid? ParentGenreId { get; set; }
+    public Genre? ParentGenre { get; set; }
+    public ICollection<Genre>? SubGenres { get; set; }
+    public ICollection<GameGenre> GenreGames { get; set; }
+}
